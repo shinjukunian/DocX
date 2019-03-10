81d35c27c2bc2c2cd8a5786cefe329470c45356f
Morten Bertz <morten@telethon.jp>
2019-03-10 17:31:20 +0900

    Initial Commit

---
+++ DocX/DocX.h
@@ -0,0 +1,19 @@
+//
+//  DocX.h
+//  DocX
+//
+//  Created by Morten Bertz on 2019/03/10.
+//  Copyright © 2019 telethon k.k. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DocX.
+FOUNDATION_EXPORT double DocXVersionNumber;
+
+//! Project version string for DocX.
+FOUNDATION_EXPORT const unsigned char DocXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocX/PublicHeader.h>
+
+
